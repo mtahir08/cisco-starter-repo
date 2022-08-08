01e1ba70621d5d212fcbf979da9ad11c6e4a32e2
Muhammad Tahir <mtahir.ps@gmail.com>
2022-08-08 15:06:33 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome tovReact
+        </h2>
+        <p>This is Tahir</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
